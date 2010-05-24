73ea47e8bcffe154877743840dc8a79f3fcff88d
TheDeveloper <TheDeveloper@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-24 11:08:17 +0000

    Updated descriptions
    git-svn-id: http://plugins.svn.wordpress.org/gosquared-livestats/trunk@244992 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gstc.php
+++ gstc.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: GoSquared LiveStats
 Plugin URI: http://www.gosquared.com/
-Description: This plugin loads the GoSquared Tracking Code for LiveStats
+Description: The official GoSquared Worpdress plugin to load the Tracking Code for GoSquared applications
 Version: 0.1
 Author: Geoff Wagstaff
 Author URI: http://www.gosquared.com/about/

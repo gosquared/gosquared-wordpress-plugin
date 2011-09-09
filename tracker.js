95de00a16d4f12a39a7b0679bc01c7417884d3cc
Geoff Wagstaff <geoff@gosquared.com>
2011-09-09 12:00:43 +0100

    Commit version 0.2.2

---
+++ tracker.js
@@ -0,0 +1,9 @@
+(function(){
+  function loadGoSquared() {
+    window._gstc_s=(new Date()).getTime();
+    var g = document.createElement('script');
+    g.src = ("https:"==document.location.protocol?"https://d1l6p2sc9645hc.cloudfront.net/":"http://d1l6p2sc9645hc.cloudfront.net/")+"tracker.js";
+    document.body.appendChild(g);
+  }
+  window.addEventListener?window.addEventListener("load",loadGoSquared,false):window.attachEvent("onload",loadGoSquared);
+})();
